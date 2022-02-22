15044d9bb9011bdd7df1f69fc6b950e7cdd05498
David Ferrero <ferrero_d@hotmail.com>
2022-02-22 11:37:55 +0100

    ;sg d4erreur non vqlide

--- js/modal.js
+++ js/modal.js
@@ -13,6 +13,18 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".form-data");
 const modalClose = document.querySelector(".close");
 
+const formFirst = document.getElementById('firstName'); // Get firstname from form
+const formLast = document.getElementById('lastName'); // Get lastname from form
+const formEmail = document.getElementById('emailAddress'); // Get email from form
+const formBirthdate = document.getElementById('birthDate'); // Get birthdate from form
+const formQuantity = document.getElementById('pastTournament'); // Get quantity of number of tournament participated from form
+let formLocation = document.querySelector('input[name="location"]:checked'); // Get location of next tournament input with let
+const formTermsConditions = document.getElementById('checkbox1'); // Get terms conditions input checkbox
+
+const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+let formIsValid; // initialize form validation;
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -29,50 +41,110 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// Ajoute un message d'erreur
+function addFormErrorMessage(element, errorMessage){
+  element.parentElement.setAttribute('data-error', errorMessage);
+  element.parentElement.setAttribute('data-error-visible', 'true');
+}
+
+// Supprime le message d'erreur
+function removeFormErrorMessage(element){ 
+  element.parentElement.removeAttribute('data-error');
+  element.parentElement.removeAttribute('data-error-visible');
+}
+
+
+/*
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+})
+*/
+
+function formFirstIsValid(){
+  if(formFirst.value == "" || formFirst.value.length < 2){
+    addFormErrorMessage(formFirst, "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+    formIsValid = false;
+  } else{
+    removeFormErrorMessage(formFirst);
+  }
+}
+
+function formLastIsValid(){
+  if(formLast.value == "" || formLast.value.length < 2){
+    addFormErrorMessage(formLast, "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+    formIsValid = false;
+  } else{
+    removeFormErrorMessage(formLast);
+  }
+}
+
+function formEmailIsValid(){
+  if (regexEmail.test(formEmail.value) != true){
+    addFormErrorMessage(formEmail, "Veuillez entrer une adresse email valide.");
+    formIsValid = false;
+  } else{
+    removeFormErrorMessage(formEmail);
+  }
+}
+
+function formBirthdateIsValid(){
+  if(formBirthdate.value == ""){
+    addFormErrorMessage(formBirthdate, "Vous devez entrer votre date de naissance.");
+    formIsValid = false;
+  } else {
+    removeFormErrorMessage(formBirthdate);
+  }
+}
+
+function formQuantityIsValid(){
+  if(formQuantity.value == "" || isNaN(formQuantity.value)){
+    addFormErrorMessage(formQuantity, "Veuillez entrer un nombre.");
+    formIsValid = false;
+  } else{
+    removeFormErrorMessage(formQuantity);
+  }
+}
+
+function formLocationIsValid(){
+  if(formLocationCheck == null){
+    addFormErrorMessage(formLocation, "Vous devez choisir une option.");
+    formIsValid = false;
+  } else{
+    removeFormErrorMessage(formLocation);
+  }
+}
+
+function formTermsConditionsIsValid(){
+  if(!formTermsConditions.checked){
+    addFormErrorMessage(formTermsConditions, "Vous devez vérifier que vous acceptez les termes et conditions.");
+    formIsValid = false;
+  } else{
+    removeFormErrorMessage(formTermsConditions);
+  }
+}
+
+// Valide toutes les informations avant envoi
 function validate(event){
-  //Anti refresh
+  //Prevent to submit
   event.preventDefault();
 
-  const formFirst = document.getElementById('firstName'); // Get firstname from form
-  const formLast = document.getElementById('lastName'); // Get lastname from form
-  const formEmail = document.getElementById('emailAddress'); // Get email from form
-  const formBirthdate = document.getElementById('birthDate'); // Get birthdate from form
-  const formQuantity = document.getElementById('pastTournament'); // Get quantity of number of tournament participated from form
-  const formLocation = document.querySelector('input[name="location"]:checked'); // Get location of next tournament input radio check
-  const formTermsConditions = document.getElementById('checkbox1'); // Get terms conditions input checkbox
+  // Refresh form location check each time we submit
+  formLocationCheck = document.querySelector('input[name="location"]:checked');
+  formIsValid = true;
 
-  const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  formFirstIsValid();
+  formLastIsValid();
+  formEmailIsValid();
+  formBirthdateIsValid();
+  formQuantityIsValid();
+  formLocationIsValid();
+  formTermsConditionsIsValid();
 
-  if(formFirst.value == "" || formFirst.value.length < 2){
-    console.log('Problème firstName');
+  if(formIsValid){
+    console.log("Inscription envoyée");
+    return true;
+  } else{
+    console.error("Attention! Une erreur s'est produite");
     return false;
   }
-
-  if(formLast.value == "" || formLast.value.length < 2){
-    console.log('Problème lastname');
-    return false;
-  }
-
-  if (regexEmail.test(formEmail.value) != true){
-    console.log('Problème adresse email');
-    return false;
-  }
-
-  if(isNaN(formQuantity.value)){
-    console.log("Problème quantité tournois n'est pas un nombre");
-    return false;
-  }
-
-  if(formLocation == null){
-    console.log('Problème aucune location est cochée');
-    return false;
-  }
-
-  if(!formTermsConditions.checked){
-    console.log('Problème la case conditions générales est non cochée');
-    return false;
-  }
-
-  console.log("Inscription envoyée");
-  return true;
 }
